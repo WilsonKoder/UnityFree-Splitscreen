d1aa96968537516148e62e6bf168cb25b9c2db66
WilsonKoder111 <wilson.koder.111@gmail.com>
2014-12-06 14:29:31 +0800

    init

---
+++ Assets/Scripts/Interface.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Interface : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void OnGUI()
+	{
+		GUI.Label (new Rect(20, 10, 100, 20), "Player 1");
+		GUI.Label (new Rect(20, Screen.height / 2 + 10, 100, 20), "Player 2");
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(gameObject.tag == "player01")
+		{
+			if(Input.GetKey (KeyCode.A))
+			{
+				rigidbody.velocity = new Vector3(-5, rigidbody.velocity.y, 0);
+			}
+			if(Input.GetKey (KeyCode.D))
+			{
+				rigidbody.velocity = new Vector3(5, rigidbody.velocity.y, 0);
+			}
+			if(Input.GetKeyDown (KeyCode.W))
+			{
+				rigidbody.velocity = new Vector3(rigidbody.velocity.x, 5, 0);
+			}
+		}
+		else if(gameObject.tag == "player02")
+		{
+			if(Input.GetKey(KeyCode.LeftArrow))
+			{
+				rigidbody.velocity = new Vector3(-5, rigidbody.velocity.y, 0);
+			}
+			if(Input.GetKey(KeyCode.RightArrow))
+			{
+				rigidbody.velocity = new Vector3(5, rigidbody.velocity.y, 0);
+			}
+			if(Input.GetKey(KeyCode.UpArrow))
+			{
+				rigidbody.velocity = new Vector3(rigidbody.velocity.x, 5, 0);
+			}
+		}
+	}
+}
